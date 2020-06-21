b32bbc6f02c75c9ac4baf8c2cba945ef7d5ac53d
Danilo Santos <danilosantos07@protonmail.com>
2020-06-21 20:52:44 -0300

    CameraService: Remove unused parameter 'kind'

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -2047,7 +2047,7 @@ void CameraService::increaseSoundRef() {
     mSoundRef++;
 }
 
-void CameraService::loadSoundLocked(sound_kind kind) {
+void CameraService::loadSoundLocked(sound_kind) {
     ATRACE_CALL();
 
     LOG1("CameraService::loadSoundLocked ref=%d", mSoundRef);
